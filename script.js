44b6686b204e8b7df10256706233fa5ee7b3e03a
Alex <s.tsamalashvili1998@gmail.com>
2022-08-17 12:00:18 +0400

    added min height function for mouse scroll

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
 const header = document.getElementById("header");
 const scrollSection = document.getElementById("scrollSection")
-let yCoordinate ;
-let clicked = 0;
+const body = document.getElementById("body")
+const textSection = document.getElementById("textSection")
+const screenSize = document.getElementById("main-window").scrollHeight
+
+ 
+
+
 
 function getCoordinate (target){
-    yCoordinate = target.y;
-    scrollSection.style.marginTop = yCoordinate+'px'
+    let yCoordinate = target.y;
+    if(yCoordinate<=screenSize-header.offsetHeight-5&&yCoordinate>=0){
+        scrollSection.style.marginTop = yCoordinate+'px';
+        let paragraphWidth =screenSize-30-yCoordinate-header.offsetHeight;
+        textSection.style.height = (paragraphWidth)+"px";
+        console.log(yCoordinate<=screenSize-header.offsetHeight-5&&yCoordinate>=0)
+    }
+    
+   
 }
 
 
 
 scrollSection.addEventListener("mousedown", (event)=>{
-    
-        window.addEventListener("mousemove",getCoordinate);
-        
-    
+    window.addEventListener("mousemove",getCoordinate);
 })
 
 scrollSection.addEventListener("mouseup", (event)=>{
     window.removeEventListener("mousemove",getCoordinate)
-    
 })
+
+
+
 
 
